2fa18aefbf469102f6fb5a1e66aee458a525e026
ermolalex <alex@kik-soft.ru>
2025-03-27 14:54:09 +0300

    add qt gui

--- ui_qt/order_form.py
+++ ui_qt/order_form.py
@@ -3,14 +3,18 @@ from mopyx import model, render, render_call, action
 
 from ui_qt.order_form_ui import Ui_Form
 from models import Order
+import commands
 from db import search
 
 
 class OrderForm(QWidget, Ui_Form):
-    def __init__(self) -> None:
+    def __init__(self, app, order: Order) -> None:
         super(OrderForm, self).__init__()
 
         self.setupUi(self)
+
+        self.app = app
+        self.order = order
 
         self.btn_add.clicked.connect(self.add_item)
         self.btn_change_qty.clicked.connect(self.change_qty)
@@ -23,7 +27,7 @@ class OrderForm(QWidget, Ui_Form):
 
         self.btn_open_sub = QPushButton("Open Sub")
         self.verticalLayout_2.addWidget(self.btn_open_sub)
-        self.btn_open_sub.clicked.connect(self.open_sub)
+        # self.btn_open_sub.clicked.connect(self.open_sub)
 
 
         self.show()
@@ -34,13 +38,13 @@ class OrderForm(QWidget, Ui_Form):
     def update_from_model(self):
         render_call(
             lambda: self.label_total.setText(
-                f"Итого: {order.total}"))
+                f"Итого: {self.order.total}"))
         self.update_table()
         self.update_buttons()
 
     @render(ignore_updates=True)
     def update_table(self):
-        items = order.items
+        items = self.order.items
         self.tableWidget.setRowCount(len(items))
         self.tableWidget.setColumnCount(3)
 
@@ -49,56 +53,37 @@ class OrderForm(QWidget, Ui_Form):
             self.tableWidget.setItem(i, 1, QTableWidgetItem(str(item.unit_price)))
             self.tableWidget.setItem(i, 2, QTableWidgetItem(str(item.qty)))
 
-        render_call(lambda: self.tableWidget.selectRow(order.selectedItemIndex),
-                    ignore_updates=True)
+        # render_call(lambda: self.tableWidget.selectRow(self.order.selectedItemIndex),
+        #             ignore_updates=True)
 
 
     @render(ignore_updates=True)
     def update_buttons(self):
-        self.btn_change_qty.setEnabled(order.selectedItemIndex >= 0)
-        self.btn_cancel.setEnabled(len(order.items) > 0)
+        # self.btn_change_qty.setEnabled(self.order.selectedItemIndex >= 0)
+        self.btn_change_qty.setEnabled(self.order.length() > 0)
+        self.btn_cancel.setEnabled(self.order.length() > 0)
 
-    @action
-    def set_selected_item_index(self):
-        order.selectedItemIndex = self.tableWidget.currentRow()
+    # @action
+    # def set_selected_item_index(self):
+    #     self.order.selectedItemIndex = self.tableWidget.currentRow()
 
     def add_item(self):
-        plu = int(self.lineEdit.text())
-        if plu:
-            current_line = order.add_line(plu)
-            print(f"добавлен товар")
-
-        self.lineEdit.setText("")
-        return
+        self.app.execute(commands.AddProductByPlu(order=self.order, user_input=self.user_input.text(), title="Publish the tutorial"))
 
     #@action
     def change_qty(self):
-        qty = int(self.lineEdit.text())
-        if qty:
-            item = order.items[order.selectedItemIndex]
-            item.qty = qty
-            order.items[order.selectedItemIndex] = item
+        ...
 
     def cancel_order(self):
-        order.items = []
+        ...
 
     def set_product(self):
-        curr_index = order.selectedItemIndex
-        item = order.items[curr_index]
-        print(item.item_total)
+        ...
+        # curr_index = self.order.selectedItemIndex
+        # item = self.order.items[curr_index]
+        # print(item.item_total)
 
-    def open_sub(self):
-        sub_form = SubForm(order)
-        sub_form.setModal(False)
-        sub_form.show()
-
-def main():
-    app = QApplication(sys.argv)
-    of = OrderForm()
-
-    ret = app.exec_()
-    sys.exit(ret)
-
-
-if __name__ == '__main__':
-    main()
+    # def open_sub(self):
+    #     sub_form = SubForm(order)
+    #     sub_form.setModal(False)
+    #     sub_form.show()
